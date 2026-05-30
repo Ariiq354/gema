fc9edb462bf5d9ed23195481d2e4925eb15a4e27
Ariiq354 <realgoldcraft@gmail.com>
2026-05-30 20:33:08 +0700

    feat: add server stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['nuxt-security', '@nuxt/image', '@nuxt/ui']
-})
+  modules: ["nuxt-security", "@nuxt/image", "@nuxt/ui", "@nuxt/eslint"],
+
+  css: ["~/assets/css/main.css"],
+
+  ui: {
+    colorMode: false,
+  },
+
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+});
---
+++ server/database/index.ts
@@ -0,0 +1,14 @@
+
+import { drizzle } from "drizzle-orm/neon-http";
+import { env } from "../../shared/env";
+import * as auth from "./schema/auth";
+
+export const db = drizzle({
+  connection: {
+    connectionString: env.DATABASE_URL,
+  },
+  schema: {
+    ...auth,
+  },
+  casing: "snake_case",
+});
