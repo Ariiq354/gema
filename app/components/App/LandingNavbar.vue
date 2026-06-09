309ce864c4ef8de0f91a8efe351add013d4000a7
Ariiq354 <realgoldcraft@gmail.com>
2026-06-09 15:25:42 +0700

    feat: custom css

--- app/components/App/LandingNavbar.vue
+++ app/components/App/LandingNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="fixed top-4 left-1/2 -translate-x-1/2 w-360 h-18 flex items-center justify-between z-20">
     <div class="relative w-full h-full ">
-      <div class="absolute w-full h-full border-2 border-eucalyptus-700 rounded-xl bg-white/10  backdrop-blur-[6px]" />
+      <div class="absolute w-full h-full border-2 border-primary-700 rounded-xl bg-white/10 backdrop-blur-[6px]" />
       <div class="absolute inset-0 flex items-center justify-between px-6">
         <div class="flex items-center gap-4">
           <NuxtImg src="/images/logo-kemenag-bogor.png" height="50" />
@@ -26,7 +26,7 @@
         </div>
 
         <div>
-          <UButton to="/login" class="bg-eucalyptus-700 hover:bg-eucalyptus-800 rounded-lg text-white py-2 px-6 cursor-pointer">
+          <UButton to="/login" class="rounded-lg py-2 px-6 cursor-pointer">
             Masuk
           </UButton>
         </div>
